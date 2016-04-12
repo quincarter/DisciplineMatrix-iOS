379873c942dc70e88c6d0fba337ce26d611887f9
quincarter <quin.carter@gmail.com>
2016-04-12 11:01:04 -0500

    Initial Creation
    This was the initial creation of the program

---
+++ DisciplineMatrix/DisciplineMatrix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DisciplineMatrix
+//
+//  Created by Carter, Lathan on 4/12/16.
+//  Copyright © 2016 LISD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DisciplineMatrix/DisciplineMatrixTests/DisciplineMatrixTests.swift
@@ -0,0 +1,36 @@
+//
+//  DisciplineMatrixTests.swift
+//  DisciplineMatrixTests
+//
+//  Created by Carter, Lathan on 4/12/16.
+//  Copyright © 2016 LISD. All rights reserved.
+//
+
+import XCTest
+@testable import DisciplineMatrix
+
+class DisciplineMatrixTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
